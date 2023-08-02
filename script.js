07bcafca626d8c94165819f66301775ef0b341e8
Botir <btrghstk@gmail.com>
2023-08-02 14:23:03 +0300

    commit my solution

--- script.js
+++ script.js
@@ -1,57 +1,72 @@
 function computerPlay() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        return "It\'s a draw!";
-    }else if
-        ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+        return "It's a draw!";
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')) {
-            return "You win!";
-    }else{
+        return "You win!";
+    } else {
         return "You lose!";
     }
 }
 
+function displayGameResult(playerWins, computerWins) {
+    if(playerWins > computerWins){
+        console.log("You win the game!");
+    } else if(playerWins < computerWins){
+        console.log("You lose the game!");
+    } else{
+        console.log("It's a draw!");
+    }
+}
+
+function isValidChoice(choice) {
+    const validChoices = ['rock', 'paper', 'scissors'];
+    return validChoices.includes(choice.toLowerCase());
+}
+
+
 function game() {
     console.log("Welcome to the game");
-	// TODO game starts flavour text (get player name?)
-	let playerWins = 0;
-	let computerWins = 0;
-	let gameLength = 4;
-    for (let i = 0; i <= gameLength; i++) {
-        let playerSelection = prompt(); // TODO input validation & error handling (add a timer?)
+    let playerReady = prompt("Are you ready? (y/n): ").toLowerCase();
+        if (playerReady === 'n') {
+            console.log("Bye!");
+            return;
+        }
 
-        if (playerSelection.toLowerCase() !== 'rock' && playerSelection.toLowerCase() !== 'paper' && playerSelection.toLowerCase() !== 'scissors') {
-            alert('Invalid input, try again');
+    let playerWins = 0;
+    let computerWins = 0;
+    let gameLength = 5;
+
+    for (let i = 0; i < gameLength; i++) {
+        let playerSelection = prompt("Enter rock, paper, or scissors: ").toLowerCase();
+
+        if (!isValidChoice(playerSelection)) {
+            console.log('Invalid input, try again');
+            i--;
             continue;
         }
 
         let computerSelection = computerPlay();
-        let gameOutcome = playRound(playerSelection, computerSelection);
-		if(gameOutcome.includes('win')){
+        let roundResult = playRound(playerSelection, computerSelection);
+        console.log(roundResult);
+
+        if(roundResult.includes('win')){
+            console.log("You win this round!")
             playerWins++;
-        }else if(gameOutcome.includes('lose')){
+        } else if(roundResult.includes('lose')){
+            console.log("You lose this round!")
             computerWins++;
         }
-        console.log(gameOutcome);
-
     }
 
-    if(playerWins > computerWins){
-        console.log("You win the game!");
-
-    }else if(playerWins < computerWins){
-        console.log("You lose the game!");
-    }else{
-        console.log("It's a draw!");
-    }
-    
+    displayGameResult(playerWins, computerWins);
 }
 
 game();
