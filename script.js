6bf03fdbf277f05377405dc3d99e2cf3a61b99ab
Marcos <49621587+marcos-lindmayer@users.noreply.github.com>
2023-08-02 14:13:06 +0200

    apply botir's playRound and validation + end game

--- script.js
+++ script.js
@@ -1,71 +1,75 @@
 function computerPlay() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    const gameOutcome = {
-		text: "",
-		winner: "",
-	};
-    // TODO logic that compares playerSelection to computerSelection
-	if (playerSelection == computerSelection) {
-		gameOutcome.text = "The round is a draw! You both picked " + playerSelection;
-		gameOutcome.winner = "draw";
-	} else {
-		switch (playerSelection) {
-			case "Rock":
-				if (computerSelection == "Paper") {
-					gameOutcome.winner = "computer";
-				}
-				break;
-			case "Paper":
-				if (computerSelection == "Scissors") {
-					gameOutcome.winner = "computer";
-				}	
-				break;
-			case "Scissors":
-				if (computerSelection == "Rock") {
-					gameOutcome.winner = "computer";
-				}
-				break;
-		}
-		if (gameOutcome.winner == "computer") {
-			gameOutcome.text = `You lost the round! ${computerSelection} beats ${playerSelection}!`;
-		} else {
-			gameOutcome.text = `You won the round! ${playerSelection} beats ${computerSelection}!`;
-			gameOutcome.winner = "player";
-		}
-	}
-    return gameOutcome;
+    if (playerSelection === computerSelection) {
+        return "It's a draw!";
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        return "You win!";
+    } else {
+        return "You lose!";
+    }
+}
+
+function isValidChoice(choice) {
+    const validChoices = ['rock', 'paper', 'scissors'];
+    return validChoices.includes(choice.toLowerCase());
 }
 
 function game() {
-    console.log("Welcome to the game");
 	// TODO game starts flavour text (get player name?)
 	let playerWins = 0;
 	let computerWins = 0;
 	let draws = 0;
 	const gameLength = 4;
     for (let i = 0; i <= gameLength; i++) {
-        let playerSelection = prompt(); // TODO input validation & error handling (add a timer?)
+		console.log("--------------");
+		console.log("Round " + (i+1));
+        let playerSelection = prompt("Type either rock, paper, or scissors!"); // TODO input validation & error handling (add a timer?)
+		if (!isValidChoice(playerSelection)) {
+            console.log('Invalid option! Options are rock, paper, or scissors.');
+            i--;
+            continue;
+        }
         let computerSelection = computerPlay();
 		console.log("Computer picked " + computerSelection);
-        let gameOutcome = playRound(playerSelection, computerSelection);
-		switch (gameOutcome.winner) {
-			case "player":
-				playerWins++;
-				break;
-			case "computer":
-				computerWins++;
-				break;
-			case "draw":
-				draws++;
-				break;
+        let roundResult = playRound(playerSelection, computerSelection);
+        if (roundResult.includes('win')) {
+            console.log("You win this round!");
+            playerWins++;
+        } else if (roundResult.includes('lose')) {
+            console.log("You lose this round!");
+            computerWins++;
+        } else {
+            console.log("Looks like a draw!");
+			draws++;
 		}
-        console.log(gameOutcome.text);
     }
-    // TODO game end logic
+	console.log("--------------");
+	// Ending game
+	if (playerWins > computerWins) {
+		console.log("Congratulations! You have won!");
+	} else if (computerWins > playerWins) {
+		console.log("You lost :(");
+	} else {
+		console.log("It is a draw!");
+	}
+	console.log("The final score is:");
+	console.log("You: " + playerWins);
+	console.log("Computer: " + computerWins);
+	console.log(draws != 0 ? "Draws: " + draws : "");
+	console.log("--------------");
+	console.log("--------------");
+	let playAgain = prompt("Want to play again? y/n"); //TODO Validation
+	if (playAgain = "y") {
+		game();
+	}
 }
 
+console.log("Welcome to the game");
 game();
