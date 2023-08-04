fade7b39dbf107df5fdef4dda02a9c0e9fd9212c
[SabaNadiradze] <nadiradzes96@gmail.com>
2023-08-04 12:40:09 +0300

    Added playRound function

--- script.js
+++ script.js
@@ -3,34 +3,44 @@ function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+
 function playRound(playerSelection, computerSelection) {
-    let gameOutcome;
-    // TODO logic that compares playerSelection to computerSelection
-    return gameOutcome, winner // "You Lose! Paper beats Rock", "computer"
+    if (playerSelection === computerSelection) {
+        return "It's a draw!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+        return "You win the round!";
+    } else {
+        return "You lose the round!";
+    }
 }
+
 
 function game() {
     console.log("Welcome to the game");
-	// TODO game starts flavour text (get player name?)
-	let playerWins = 0;
-	let computerWins = 0;
-	let draws = 0;
-	let gameLength = 4;
+    // TODO game starts flavour text (get player name?)
+    let playerWins = 0;
+    let computerWins = 0;
+    let draws = 0;
+    let gameLength = 4;
     for (let i = 0; i <= gameLength; i++) {
         let playerSelection = prompt(); // TODO input validation & error handling (add a timer?)
         let computerSelection = computerPlay();
         let gameOutcome, winner = playRound(playerSelection, computerSelection);
-		switch (winner) {
-			case "player":
-				playerWins++
-				break;
-			case "computer":
-				computerWins++
-				break;
-			default:
-				draws++
-				break;
-		}
+        switch (winner) {
+            case "player":
+                playerWins++
+                break;
+            case "computer":
+                computerWins++
+                break;
+            default:
+                draws++
+                break;
+        }
         console.log(gameOutcome);
     }
     // TODO game end logic
