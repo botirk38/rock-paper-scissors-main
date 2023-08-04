eb1d55d6d9bfc7a7c7e83f2fa39e8ad011871752
[SabaNadiradze] <nadiradzes96@gmail.com>
2023-08-04 13:17:43 +0300

    Fixed some bugs

--- script.js
+++ script.js
@@ -17,16 +17,16 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function displayGameResult(playerWins, computerWins) {
+function displayGameResult(playerWins, computerWins, draw) {
     if (playerWins > computerWins) {
         console.log("You win the game!");
-        console.log("Score " + playerWins + " : " + computerWins);
+        console.log("Score Player " + playerWins + " : Computer " + computerWins);
     } else if (playerWins < computerWins) {
         console.log("You lose the game!");
-        console.log("Score " + computerWins + " : " + playerWins);
+        console.log("Score Computer " + computerWins + " : Player " + playerWins);
     } else {
         console.log("It's a draw!");
-        console.log("Score " + playerWins + " : " + computerWins);
+        console.log(draw + " times!");
     }
 }
 
@@ -44,36 +44,32 @@ function game() {
         return;
     }
 
+    let draw = 0;
     let playerWins = 0;
     let computerWins = 0;
-    let gameLength = 5;
+    let gameLength = 4;
 
     for (let i = 0; i < gameLength; i++) {
         let playerSelection = prompt("Enter rock, paper, or scissors: ").toLowerCase();
-
         if (!isValidChoice(playerSelection)) {
             console.log('Invalid input, try again');
             i--;
             continue;
         }
-
         let computerSelection = computerPlay();
         let roundResult = playRound(playerSelection, computerSelection);
         console.log(roundResult);
 
         if (roundResult.includes('win')) {
-            // console.log("You win this round!")
             playerWins++;
         } else if (roundResult.includes('lose')) {
-            // console.log("You lose this round!")
             computerWins++;
         } else {
-            playerWins++;
-            computerWins++;
+            draw++
         }
     }
-
-    displayGameResult(playerWins, computerWins);
+    displayGameResult(playerWins, computerWins, draw);
+ 
 }
 
 game();
